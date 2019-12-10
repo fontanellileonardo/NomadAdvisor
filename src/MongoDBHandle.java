5ef3046c798534028a78bb6c7fa4facf585499b1
Leonardo Fontanelli <l.fontanelli2@studenti.unipi.it>
2019-12-10 16:15:14 +0100

    Connessione con MongoDB funzionante

--- src/MongoDBHandle.java
+++ src/MongoDBHandle.java
@@ -1,5 +1,19 @@
-import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 public class MongoDBHandle {
+
+    private static MongoClient mongoClient;
+
+    static{
+        mongoClient = MongoClients.create("mongodb://127.0.0.1:27017");
+        System.out.println("Created Connection with the DB");
+    }
+
+    public static void finish() {
+        if(mongoClient != null)
+            mongoClient.close();
+            System.out.println("Closed connection with the DB");
+    }
 
 }
