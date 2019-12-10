a895b6351fcbb8a800a8396120f05a0fd21ad2a1
Carmelo Aparo <carmelo.aparo.ca@gmail.com>
2019-12-10 18:21:42 +0100

    Created Singleton class

--- src/MongoDBHandle.java
+++ src/MongoDBHandle.java
@@ -8,7 +8,7 @@ public class MongoDBHandle {
     private static MongoClient mongoClient;
 
     static{
-        mongoClient = MongoClients.create("mongodb://127.0.0.1:27017");
+        mongoClient = DBConnection.getInstance().mongoClient;
         System.out.println("Created Connection with the DB");
     }
 
