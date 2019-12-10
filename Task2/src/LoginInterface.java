583738d486ef34b1d8b7ac834b9413a8b1978a81
geggi <geggi@mbpdieugenia2.station>
2019-12-10 13:14:48 +0100

    E si ricomincia

---
+++ Task2/src/LoginInterface.java
@@ -0,0 +1,64 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+
+public class LoginInterface {
+
+    @FXML
+    private Separator separator;
+
+    @FXML
+    private Label logLablel;
+
+    @FXML
+    private Label regLabel;
+
+    @FXML
+    private PasswordField pwdField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private Label pedLabel;
+
+    @FXML
+    private Button logButton;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField surnameField;
+
+    @FXML
+    private TextField nickNameField;
+
+    @FXML
+    private TextField emailRegField;
+
+    @FXML
+    private TextField pwdRegField;
+
+    @FXML
+    private Button regButton;
+
+    @FXML
+    void login(ActionEvent event) {
+    	System.out.println("Funzionaaaa!!");
+    }
+
+    @FXML
+    void registration(ActionEvent event) {
+
+    }
+
+}
+
---
+++ Task2/src/NomadAdvisor.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+//import javafx.event.*;
+//import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+//import javafx.scene.control.*;
+//import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+
+public class NomadAdvisor extends Application {
+    
+    private LoginInterface loginInterface;
+    private Stage stage;
+    private Parent root;
+    
+    
+    
+    public void start(Stage stage) {
+        this.stage = stage;
+        
+        this.stage.setTitle("Nomad Advisor");
+        try {
+			this.root = FXMLLoader.load(NomadAdvisor.class.getResource("resources/LoginInterface.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        this.stage.setScene(new Scene(root));
+        this.stage.show();
+    }
+}
+
