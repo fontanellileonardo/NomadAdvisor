0264e91e8dd91e3c28a4049a910ec352cad90c1a
Carmelo Aparo <carmelo.aparo.ca@gmail.com>
2019-12-13 20:28:23 +0100

    Removed PREFERENCES from cities attributes map

--- src/Utils.java
+++ src/Utils.java
@@ -6,13 +6,12 @@ public class Utils {
 	// City's Characteristics
 	public static enum cityNames {
 		TEMPERATURE,COST,AIR_QUALITY,SAFETY,QUALITY_LIFE,WALKABILITY,HEALTHCARE,NIGHTLIFE,
-		WIFI,FOREIGNERS,ENGLISH,PREFERENCES
+		WIFI,FOREIGNERS,ENGLISH
 	}
 	// HashMap where the key is the cityNames and the value is the key in the DB related to the characteristics
 	public static HashMap<cityNames,String> cityAttributes = new HashMap();
 
 	static {
-		cityAttributes.put(cityNames.PREFERENCES,"preferences");
 		cityAttributes.put(cityNames.TEMPERATURE,"temperature");
 		cityAttributes.put(cityNames.COST,"cost");
 		cityAttributes.put(cityNames.AIR_QUALITY,"airQuality");
@@ -24,14 +23,14 @@ public class Utils {
 		cityAttributes.put(cityNames.WIFI, "wifi");
 		cityAttributes.put(cityNames.FOREIGNERS, "foreignersFriendly");
 		cityAttributes.put(cityNames.ENGLISH, "english");
-		cityAttributes.put(cityNames.PREFERENCES, "preferences");
 	}
 	
 	// DB's keys
 	public final static String ID = "_id",
 			// city's keys
 			CITY = "city",
-			COUNTRY = "country";
+			COUNTRY = "country",
+			PREFERENCES = "preferences";
 	
 	// encrypt the password
     public static String cryptPwd(String pwd) {
