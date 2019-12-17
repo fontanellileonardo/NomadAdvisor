d0d278902e3be7953748d45b025cf419b3d5eb3e
geggi <geggi@host-131-114-209-171.m.unipi.it>
2019-12-17 19:15:43 +0100

    Corretti piccoli errori

--- src/NomadAdvisor.java
+++ src/NomadAdvisor.java
@@ -20,19 +20,18 @@ public class NomadAdvisor extends Application {
     private HotelInterface hotelInterface;
     private PersonalAreaInterface personalAreaInterface;
     private CityInterface cityInterface;
-    private EmployeeInterface employeeInterface;
     private Stage stage;
-    private Parent root;
     private FXMLLoader fxmlLoaderHotel;
     private FXMLLoader fxmlLoaderLogin;
     private FXMLLoader fxmlLoaderCity;
-    private FXMLLoader fxmlLoaderEmployee;
+    private FXMLLoader fxmlLoaderPersonalArea;
     private NomadHandler nomadHandler;
     private Scene loginScene;
+    private Scene personalAreaScene;
     private Scene cityScene;
     private Scene hotelScene;
-    private Scene employeeScene;
     private User loggedUser;
+    private City selectedCity;
 
     public NomadHandler getNomadHandler() {
         return nomadHandler;
@@ -46,28 +45,32 @@ public class NomadAdvisor extends Application {
         	// Initialize FXML loaders
         	fxmlLoaderCity = new FXMLLoader(NomadAdvisor.class.getResource("resources/CityInterface.fxml"));
         	fxmlLoaderLogin = new FXMLLoader(NomadAdvisor.class.getResource("resources/LoginInterface.fxml"));
-        	fxmlLoaderEmployee = new FXMLLoader(NomadAdvisor.class.getResource("resources/EmployeeInterface.fxml"));
+        	fxmlLoaderHotel = new FXMLLoader(NomadAdvisor.class.getResource("resources/HotelInterface.fxml"));
+        	fxmlLoaderPersonalArea = new FXMLLoader(NomadAdvisor.class.getResource("resources/PersonalAreaInterface.fxml"));
         	// Create Scenes
         	this.loginScene = new Scene(fxmlLoaderLogin.load());
         	this.cityScene = new Scene(fxmlLoaderCity.load());
-        	this.employeeScene = new Scene(fxmlLoaderEmployee.load());
+        	this.hotelScene = new Scene(fxmlLoaderHotel.load());
+        	this.personalAreaScene = new Scene(fxmlLoaderPersonalArea.load());
           
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-
-    // Get controllers
-    loginInterface = (LoginInterface) fxmlLoaderLogin.getController();
-    cityInterface = (CityInterface) fxmlLoaderCity.getController();
-    employeeInterface = (EmployeeInterface) fxmlLoaderEmployee.getController();
-    // Set the NomadAdvisor reference
-    cityInterface.setNomadAdvisor(this);
-    loginInterface.setNomadAdvisor(this);
-
-    changeScene("loginInterface");
-    this.stage.show();
+	    // Get controllers
+	    loginInterface = (LoginInterface) fxmlLoaderLogin.getController();
+	    cityInterface = (CityInterface) fxmlLoaderCity.getController();
+	    hotelInterface = (HotelInterface) fxmlLoaderHotel.getController();
+	    personalAreaInterface = (PersonalAreaInterface) fxmlLoaderPersonalArea.getController();
+	    // Set the NomadAdvisor reference
+	    cityInterface.setNomadAdvisor(this);
+	    loginInterface.setNomadAdvisor(this);
+	    hotelInterface.setNomadAdvisor(this);
+	    personalAreaInterface.setNomadAdvisor(this);
+	
+	    changeScene("loginInterface");
+	    this.stage.show();
     }
 
     public void changeScene(String newScene) { //per ora solo login-interface
@@ -76,15 +79,17 @@ public class NomadAdvisor extends Application {
                 this.stage.setScene(loginScene);
                 break;
             case "personalAreaInterface":
-            	System.out.println("personal Area Interface");
+            	personalAreaInterface.initInterface();
+            	this.stage.setScene(personalAreaScene);
             	break;
             case "cityInterface":
-            	cityInterface.initialize();
+            	cityInterface.initInterface();
             	this.stage.setScene(cityScene);
             	break;
-            case "emplpoyeeInterface":
-            	employeeInterface.initialize();
-            	this.stage.setScene(employeeScene);
+            case "hotelInterface":
+            	hotelInterface.initInterface();
+            	this.stage.setScene(hotelScene);
+            	break;
             default:
                 System.out.println("Not Implemented");
         }
@@ -96,6 +101,14 @@ public class NomadAdvisor extends Application {
     
     public User getUser() {
     	return loggedUser;
+    }
+    
+    public void setCity(City city) {
+    	selectedCity = city;
+    }
+    
+    public City getCity() {
+    	return selectedCity;
     }
 }
 
